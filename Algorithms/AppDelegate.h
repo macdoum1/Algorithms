6df8aa08aab339044692d16f2d71b60c48ebb204
mmacdougall <mmacdougall@etsy.com>
2016-01-27 16:05:36 -0500

    Initial Commit

---
+++ Algorithms/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Algorithms
+//
+//  Created by Mike MacDougall on 1/27/16.
+//  Copyright © 2016 Michael MacDougall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Algorithms/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Algorithms
+//
+//  Created by Mike MacDougall on 1/27/16.
+//  Copyright © 2016 Michael MacDougall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

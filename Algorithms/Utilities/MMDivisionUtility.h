0cd18dfc0c0b989b42850238446d0b6b628b6161
Michael MacDougall <aplmik24@comcast.net>
2016-02-07 21:03:59 -0500

    division without divide operator

---
+++ Algorithms/Utilities/MMDivisionUtility.h
@@ -0,0 +1,13 @@
+//
+//  MMDivisionUtility.h
+//  MMAlgorithms
+//
+//  Created by Michael MacDougall on 2/7/16.
+//  Copyright © 2016 Michael MacDougall. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MMDivisionUtility : NSObject
++ (NSInteger)dividerNumerator:(NSInteger)numerator denominator:(NSInteger)denominator;
+@end
